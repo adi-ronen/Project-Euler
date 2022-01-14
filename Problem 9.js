b974541a1bb659802cd6f3ca16bbd29d9f7f9f48
Adi Ronen <adironenn@gmail.com>
2022-01-14 17:50:29 +0200

    Create Problem 9.js

---
+++ Problem 9.js
@@ -0,0 +1,14 @@
+const specialPythagoreanTriplet = sum => {
+  for (let a = 0; a < 1000/3; a++) {
+    for (let b = a + 1; b < 1000/2; b++) {
+      for (let c = b + 1; c < 1000; c++) {
+        if ( a+b+c==1000 && (a*a)+(b*b)==(c*c)) {
+          return [a, b, c]
+        }
+      }
+    }
+  }
+  return null
+}
+
+specialPythagoreanTriplet(1000)
